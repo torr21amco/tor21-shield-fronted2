feb5f5cc3d7929aa48a3251731766006c6adb9ab
torr21amco <tuzonaenpositivo@gmail.com>
2025-03-25 21:26:03 -0400

    Corregir errores de BOM y OWNER_ADDRESS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import { QRCodeCanvas } from 'qrcode.react';
 import './App.css';
